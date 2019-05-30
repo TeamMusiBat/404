73f7518328f6086fe7c875d465b7dcb8b7e4eb39
Denjaka <43498952+InexBots@users.noreply.github.com>
2019-05-31 01:34:29 +0700

    Add files via upload

---
+++ alat.sh
@@ -0,0 +1,45 @@
+pkg install python
+
+pkg install python3
+
+pkg install python-pip
+
+pkg install python3-pip
+
+pip3 install rsa
+
+pip3 install requests
+
+pip3 install googletrans
+
+pip3 install bs4
+
+pip3 install requests
+
+pip3 install rsa
+
+pip3 install thrift
+
+pip3 install bs4
+
+pip3 install pytz
+
+pip3 install humanfriendly
+
+pip3 install gtts
+
+pip3 install googletrans
+
+pip3 install wikipedia
+
+pip3 install youtube_dl
+
+pip3 install ffmpy
+
+pip3 install pafy
+
+pip3 install tweepy
+
+pip3 install goslate
+
+pip3 install html5lib
